1cee3e79c6f56055978bf8e94903270fb8c60d4d
Max Dobler <mdblr@outlook.com>
2016-07-05 13:40:43 -0700

    test init Haversin implementation at /api

--- routes/api.js
+++ routes/api.js
@@ -6,7 +6,8 @@ const knex = require('../db/knex');
 
 /* GET local resources */
 router.get('/', (req, res, next) => {
-  knex('locdata').select().then( data => {
+  knex.raw("SELECT * FROM (SELECT id, lat, long, (3959 * acos(cos(radians(47.598962)) * cos(radians(lat)) * cos(radians(long) - radians(-122.333799)) + sin(radians(47.598962)) * sin(radians(lat)))) AS distance FROM locdata) AS distances WHERE distance < 1 ORDER BY distance OFFSET 0 LIMIT 15;")
+  .then( data => {
     res.json(data);
   })
 });
